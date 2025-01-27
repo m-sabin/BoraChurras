e18dfcc9a516f5e2bf9fd8ee2ec5fa1624ca819d
nibass <contato.ecomcenter@gmail.com>
2025-01-27 15:15:29 -0300

    Recuperação dos dados do usuario no InfoActivity.kt Enviar e Recuperar os dados no ResultActivity.kt Inserir as formulas e calcular

--- app/src/main/java/com/example/borachurras/InfoActivity.kt
+++ app/src/main/java/com/example/borachurras/InfoActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.borachurras.databinding.ActivityInfoBinding
+import com.google.android.material.snackbar.Snackbar
 
 class InfoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityInfoBinding
@@ -16,9 +17,37 @@ class InfoActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnCalcular.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)
-        }
+            val numofadult = binding.tieNumAdult.text.toString()
+            val numofchild = binding.tieNumChild.text.toString()
+            val timing = binding.tieTime.text.toString()
 
+            if (numofadult.isEmpty() || numofchild.isEmpty() || timing.isEmpty()) {
+                Snackbar.make(
+                    binding.tieNumAdult,
+                    "Por favor, preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                ).show()
+                return@setOnClickListener
+            } else {
+                val numofadultInt = numofadult.toIntOrNull()
+                val numofchildInt = numofchild.toIntOrNull()
+                val timingInt = timing.toIntOrNull()
+
+                if (numofadultInt == null || numofchildInt == null || timingInt == null) {
+                    Snackbar.make(
+                        binding.tieNumAdult,
+                        "Por favor, insira núeros válidos em todos os campos",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    return@setOnClickListener
+                }
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("NumAdult", numofadultInt)
+                intent.putExtra("NumChild", numofchildInt)
+                intent.putExtra("Time", timingInt)
+                startActivity(intent)
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/borachurras/ResultActivity.kt
+++ app/src/main/java/com/example/borachurras/ResultActivity.kt
@@ -15,10 +15,38 @@ class ResultActivity : AppCompatActivity() {
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val numofadlt = intent.getIntExtra("NumAdult", 0)
+        val numofchild = intent.getIntExtra("NumChild", 0)
+        val timing = intent.getIntExtra("Time", 0)
+
+        val meatKg = calcMeat(numofadlt, numofchild, timing)
+        val beerLatas = calcBeer(numofadlt, timing)
+        val refrilitros = calcRefri(numofadlt, numofchild, timing)
+
+        binding.tvQtdMeat.text = "%.2f".format(meatKg)
+        binding.tvQtdBear.text = beerLatas.toString()
+        binding.tvQtdRefri.text = "%.2f".format(refrilitros)
+
         binding.btnRecalcular.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
 
+    private fun calcMeat(adult: Int, child: Int, timing: Int): Double {
+        val consumAdult = if (timing > 4) 0.6 else 0.4
+        val consumChild = consumAdult / 2
+        return (adult * consumAdult * timing) + (child * consumChild * timing)
+    }
+
+    private fun calcBeer(adult: Int, timing: Int): Int {
+        val consumAdult = if (timing > 4) 7 else 5
+        return adult * consumAdult * timing
+    }
+
+    private fun calcRefri(adult: Int, child: Int, timing: Int): Double {
+        val consumAdult = 0.5
+        val consumChild = 0.3
+        return (adult * consumAdult * timing) + (child * consumChild * timing)
     }
 }
